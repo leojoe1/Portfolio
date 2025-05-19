d2db56ae7addae85bf64e2cf1c8827022de431c8
leojoe1 <leochouarbi@gmail.com>
2025-05-20 00:36:31 +0200

    Restored vite.config.js & tailwind.config.js

--- Portfolio-main/tailwind.config.js
+++ Portfolio-main/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // <- ça prend TOUS tes composants
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
